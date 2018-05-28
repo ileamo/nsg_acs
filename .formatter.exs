fa87b9a6fc0a305e77b958530458a6b1674f05e1
Igor Mosunov <imosunov@nsg.ru>
2018-05-28 12:28:49 +0300

    Add .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,7 @@
+[
+  # functions to let allow the no parens like def print value
+  locals_without_parens: [field: :*],
+
+  # files to format
+  inputs: ["mix.exs", "{config,lib,test}/**/*.{ex,exs}"]
+]
