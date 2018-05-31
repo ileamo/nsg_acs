670e949e16d75607b327760df05e49799d699840
Igor Mosunov <imosunov@nsg.ru>
2018-05-31 18:42:41 +0300

    Отображать параметры шаблона

--- lib/nsg_acs_web/views/group_view.ex
+++ lib/nsg_acs_web/views/group_view.ex
@@ -1,3 +1,18 @@
 defmodule NsgAcsWeb.GroupView do
   use NsgAcsWeb, :view
+
+  alias NsgAcs.GroupConf
+
+  def str_head(str, len) do
+    str
+    |> String.slice(0, len)
+    |> Kernel.<>(" ...")
+  end
+
+  def get_params(templ) do
+    templ
+    |> GroupConf.get_params_from_template()
+    |> Map.keys()
+    |> Enum.join(", ")
+  end
 end
