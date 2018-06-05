bf14f68981a1a0dd971a1f87e7ac6092abfa8a06
Igor Mosunov <ileamo@yandex.ru>
2018-06-05 16:01:47 +0300

    Вывод параметров устройства

--- lib/nsg_acs_web/views/device_view.ex
+++ lib/nsg_acs_web/views/device_view.ex
@@ -1,3 +1,9 @@
 defmodule NsgAcsWeb.DeviceView do
   use NsgAcsWeb, :view
+
+  def param_list(param_map) do
+    param_map
+    |> Enum.map(fn {k, v} -> "#{k}=#{v}" end)
+    |> Enum.join(", ")
+  end
 end
