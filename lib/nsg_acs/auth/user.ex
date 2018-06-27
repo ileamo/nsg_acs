fc8c836b68368e88dcedf32b27778369974efdc0
Igor Mosunov <imosunov@nsg.ru>
2018-06-27 17:56:48 +0300

    Ввод пользователя и пароля

--- lib/nsg_acs/auth/user.ex
+++ lib/nsg_acs/auth/user.ex
@@ -2,20 +2,32 @@ defmodule NsgAcs.Auth.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-
   schema "users" do
     field :is_admin, :boolean, default: false
     field :password_hash, :string
     field :username, :string
-
+    field :password, :string, virtual: true
     timestamps()
   end
 
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :password_hash, :is_admin])
-    |> validate_required([:username, :password_hash, :is_admin])
+    |> cast(attrs, [:username, :password, :is_admin])
+    |> validate_required([:username, :password, :is_admin])
+    |> validate_length(:username, min: 3, max: 32)
+    |> validate_length(:password, min: 6, max: 32)
     |> unique_constraint(:username)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule NsgAcs.Mixfile do
       app: :nsg_acs,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -26,7 +26,7 @@ defmodule NsgAcs.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -40,7 +40,10 @@ defmodule NsgAcs.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:guardian, "~> 1.0"},
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 0.12"}
     ]
   end
 
@@ -54,7 +57,7 @@ defmodule NsgAcs.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
