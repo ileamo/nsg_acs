4c8d720df901a80f817f9c5cc17ab8b36d004da0
Igor Mosunov <imosunov@nsg.ru>
2018-07-09 14:47:47 +0300

    Исправление ошибок

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule NsgAcs.Mixfile do
       {:cowboy, "~> 1.0"},
       {:guardian, "~> 1.0"},
       {:comeonin, "~> 4.0"},
-      {:bcrypt_elixir, "~> 0.12"}
+      {:bcrypt_elixir, "~> 1.0"}
     ]
   end
 
