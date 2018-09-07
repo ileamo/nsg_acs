5e8474f5a9f01b71560f96f8942804858b2bb16c
Igor Mosunov <imosunov@nsg.ru>
2018-09-07 12:59:53 +0300

    Добавить лексер и парсер для валидации конфига NSG.

---
+++ lib/parser/nsgconf_parser.ex
@@ -0,0 +1,14 @@
+defmodule NSGconfParser do
+  def parse(str) do
+    with {:ok, tokens, _} <- :nsgconf_lexer.string(to_charlist(str)),
+         {:ok, result} <- :nsgconf_parser.parse(tokens) do
+      result
+    else
+      {:error, reason, _} ->
+        reason
+
+      {:error, {line, :nsgconf_parser, reason}} ->
+        {line, to_string(reason)}
+    end
+  end
+end
